6e8a0e164596fcf2ba706e56cbab752b71ec07b3
giannoutsosBarkas <dimitris.giannoutsos@aspisid.com>
2023-05-19 13:57:56 +0000

    weather API

--- imxweb/projects/qer/src/lib/contactinformation/contactinformation.component.ts
+++ imxweb/projects/qer/src/lib/contactinformation/contactinformation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'imx-contactinformation',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactinformationComponent implements OnInit {
 
-  constructor() { }
+  date_time: Date;
+  temperature: number;
+  wind_speed: number;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    let headers = new HttpHeaders({});
+    this.http.get<any>('https://api.open-meteo.com/v1/forecast?latitude=37.98376&longitude=23.72784&current_weather=true&hourly=temperature_2m,windspeed_10m', {headers: headers}).subscribe(data => { 
+      this.date_time = data.current_weather.time;
+      this.temperature = data.current_weather.temperature;
+      this.wind_speed = data.current_weather.windspeed;
+      console.log(data);});
   }
-
 }
