096500097820d0669486eab86683c9071fb8a174
Dimitris Barkas <dimitris.giannoutsos@aspisid.com>
2024-03-06 12:17:28 +0000

    formating changes

--- imxweb/projects/data-explorer-plus/src/lib/data-explorer-plus-details/data-explorer-plus-details.component.ts
+++ imxweb/projects/data-explorer-plus/src/lib/data-explorer-plus-details/data-explorer-plus-details.component.ts
@@ -15,16 +15,19 @@ interface ExplorerItem {
   Value: string;
   Children: ExplorerItem[];
 }
+
 interface IdentQBMLimitedSQLType {
   IdentQBMLimitedSQL: string | null;
   xKey: string | null;
   xSubKey: string | null;
 }
+
 @Component({
   selector: 'imx-data-explorer-plus-details',
   templateUrl: './data-explorer-plus-details.component.html',
   styleUrls: ['./data-explorer-plus-details.component.scss']
 })
+
 export class DataExplorerPlusDetailsComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
   @ViewChild('tabGroup') tabGroup: MatTabGroup;
@@ -40,19 +43,18 @@ export class DataExplorerPlusDetailsComponent implements OnInit {
   dataSourcedynamic: ExplorerItem[];
   searchControl = new FormControl('');
   tabsItem: ExplorerItem[] = [];
-  tabDataMapping: { [tabLabel: string]: any[] } = {
-  };
+  tabDataMapping: { [tabLabel: string]: any[] } = {};
+
   constructor(
     @Inject(EUI_SIDESHEET_DATA) public data: any,
     private readonly config: AppConfigService,
     private service: DataExplorerPlusService,
     private route: ActivatedRoute,) {
     this.lotsOfTabs = new Array(5).fill(0).map((_, index) => `Tab ${index + 1}`);
-    console.log("Received in side sheet:", data.xKey, data.xSubKey, data.configParm);
   }
 
   async ngOnInit(): Promise<void>  {
-    this.updateDataSourceForTab(this.lotsOfTabs[0]);
+    this.updateTabDataSource(this.lotsOfTabs[0]);
     await this.ExplorerList();
     this.getTabs(this.data.configParm);
   }
@@ -60,8 +62,6 @@ export class DataExplorerPlusDetailsComponent implements OnInit {
   applyFilter(): void {
     const filterValue = this.searchControl.value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    // Reset the paginator to the first page (important if you have pagination)
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
@@ -76,29 +76,27 @@ export class DataExplorerPlusDetailsComponent implements OnInit {
     this.IdentQBMLimitedSQL = null;
     
     //Get the tabs from the tree
-    const findTabs = (items: ExplorerItem[]): string | null => {
+    const getTabsParam = (items: ExplorerItem[]): string | null => {
       for (const item of items) {
         if (item.ConfigParm === configParm) {
           const detailsChildren= item.Children.find(child => child.ConfigParm === 'Details');
           this.tabsItem = detailsChildren.Children;
           return detailsChildren ? detailsChildren.Value : null;
         } else if (item.Children.length > 0) {
-          
-          const result = findTabs(item.Children);
+          const result = getTabsParam(item.Children);
           if (result) return result; 
         }
       }
       return null; // Return null if not found at all
     };
-
-    findTabs(this.dataSourcedynamic);
+    getTabsParam(this.dataSourcedynamic);
   } 
 
-  onTabChange(event: any): void {
+  onTabClick(event: any): void {
     const selectedIndex = event;
     const selectedTabLabel = this.tabGroup._tabs.toArray()[selectedIndex].textLabel;
     console.log('Selected tab:', selectedTabLabel);
-    
+
     //Find the select statement based on the tab selected by the user
     const findSelectStmt = (items: ExplorerItem[]): string | null => {
       for (const item of items) {
@@ -126,16 +124,14 @@ export class DataExplorerPlusDetailsComponent implements OnInit {
     this.executeSQL(this.IdentQBMLimitedSQL);
   }
 
-  updateDataSourceForTab(tabLabel: string): void {
+  updateTabDataSource(tabLabel: string): void {
     const dataForTab = this.tabDataMapping[tabLabel];
     if (dataForTab) {
       this.dataSource.data = dataForTab;
     } else {
-      // Handle the case where there is no data for the tab
       this.dataSource.data = [];
     }
   }
-
 
   public async ExplorerList(): Promise<void> {
     const explorers = await this.config.apiClient.processRequest<ExplorerItem[]>(this.GetExplorers());
@@ -169,9 +165,8 @@ export class DataExplorerPlusDetailsComponent implements OnInit {
         });
         return flattenedRow;
       });
-    // Set displayed columns based on the first row keys but exclude 'xKey'
-    this.displayedColumns = Object.keys(this.dataSource.data[0]).filter(key => key !== 'xKey' && key !== 'xSubKey');
-    
+      // Set displayed columns based on the first row keys but exclude 'xKey'
+      this.displayedColumns = Object.keys(this.dataSource.data[0]).filter(key => key !== 'xKey' && key !== 'xSubKey');
     }
     if (this.paginator) {
       this.paginator.firstPage();
@@ -198,5 +193,4 @@ export class DataExplorerPlusDetailsComponent implements OnInit {
       responseType: 'json',
     };
   }
-
 }
